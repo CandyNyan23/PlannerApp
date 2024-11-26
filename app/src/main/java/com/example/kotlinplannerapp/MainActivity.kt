1944eb843af922f9a59324f4bb76731a0563260f
Candela <candela6d2@gmail.com>
2024-11-26 13:11:52 +0100

    se borra edit text

--- app/src/main/java/com/example/kotlinplannerapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinplannerapp/MainActivity.kt
@@ -8,9 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    val editText: EditText? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
