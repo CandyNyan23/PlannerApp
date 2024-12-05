1cfabee55f9e1599f49decf4b76d82e7417f5981
Candela <candela6d2@gmail.com>
2024-12-05 12:38:50 +0100

    feat: Login Functions

--- app/src/main/java/com/example/kotlinplannerapp/home/HomeActivity.kt
+++ app/src/main/java/com/example/kotlinplannerapp/home/HomeActivity.kt
@@ -1,4 +1,22 @@
 package com.example.kotlinplannerapp.home
 
-class HomeActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinplannerapp.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homeScreen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
