7656e1eaf72cf1fd943341858301a5d06a9a825a
CandyNyan23 <144055303+CandyNyan23@users.noreply.github.com>
2024-12-05 12:47:20 +0100

    feat: Login Functions (#5)

--- app/src/main/java/com/example/kotlinplannerapp/login/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinplannerapp/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinplannerapp.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -16,19 +17,23 @@ import com.example.kotlinplannerapp.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    private var loginButton : Button? = null
-    private var email : EditText? = null
-    private var password : EditText? = null
+    private var loginButton: Button? = null
+    private var email: EditText? = null
+    private var password: EditText? = null
 
-    private var loginObserver : Observer<Boolean> = Observer { isLoginSuccessful ->
+    private val loginObserver: Observer<Boolean> = Observer { isLoginSuccessful ->
         if (isLoginSuccessful) {
             startActivity(Intent(this, HomeActivity::class.java))
         } else {
-            Toast.makeText(null, "Enter valid email address and/or password.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Enter valid email address and/or password.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
-    private val loginActivityViewModel :LoginActivityViewModel by viewModels()
+    private val loginActivityViewModel: LoginActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +44,22 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        loginActivityViewModel.getIsLoginSuccessLiveData().observe(this, loginObserver)
+        initObservers()
+        bindViews()
+        loginButton?.setOnClickListener(::onLoginButtonClick)
+    }
+
+    private fun onLoginButtonClick(view: View) {
+        loginActivityViewModel.login(email?.text.toString(), password?.text.toString())
+    }
+
+    private fun bindViews() {
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
-        loginButton = findViewById(R.id.loginButton)
-        loginButton?.setOnClickListener {
-            loginActivityViewModel.login(email?.text.toString(), password?.text.toString())
-        }
+        loginButton = findViewById(R.id.button_login)
+    }
+
+    private fun initObservers() {
+        loginActivityViewModel.getIsLoginSuccessLiveData().observe(this, loginObserver)
     }
 }
--- app/src/main/java/com/example/kotlinplannerapp/login/LoginActivityViewModel.kt
+++ app/src/main/java/com/example/kotlinplannerapp/login/LoginActivityViewModel.kt
@@ -1,22 +1,40 @@
 package com.example.kotlinplannerapp.login
 
 import android.util.Patterns
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class LoginActivityViewModel : ViewModel() {
 
-    private var isLoginSuccessLiveData : MutableLiveData<Boolean> = MutableLiveData()
+    private val isLoginSuccessLiveData : MutableLiveData<Boolean> = MutableLiveData()
+    private var auth: FirebaseAuth = Firebase.auth
 
     fun getIsLoginSuccessLiveData() : LiveData<Boolean> = isLoginSuccessLiveData
 
+    fun isValidPassword(password: String?): Boolean {
+
+        val pattern: Pattern
+        val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+
+        pattern = Pattern.compile(PASSWORD_PATTERN)
+        val matcher: Matcher = pattern.matcher(password.toString())
+
+        return matcher.matches()
+    }
+
     fun login(email : String, password : String) {
-        if (email.isNotEmpty() && password.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            isLoginSuccessLiveData = MutableLiveData(true)
-        } else {
-            isLoginSuccessLiveData = MutableLiveData(false)
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches() && isValidPassword(password)) {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                isLoginSuccessLiveData.postValue(it.isSuccessful)
+            }
+        } else { 
+            isLoginSuccessLiveData.postValue(false)
         }
     }
 }
