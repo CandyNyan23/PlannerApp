76209b524e732b7da6a3c0c3d6f7c2b1f4d1dbac
CandyNyan23 <144055303+CandyNyan23@users.noreply.github.com>
2024-12-13 12:47:06 +0100

    Drawer Menu + ToolBar (#7)

--- app/src/main/java/com/example/kotlinplannerapp/home/HomeActivity.kt
+++ app/src/main/java/com/example/kotlinplannerapp/home/HomeActivity.kt
@@ -1,10 +1,15 @@
 package com.example.kotlinplannerapp.home
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.kotlinplannerapp.R
 
 class HomeActivity : AppCompatActivity() {
@@ -17,6 +22,34 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val topBar: Toolbar = findViewById(R.id.topBar)
+        setSupportActionBar(topBar)
+        topBar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        topBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    true
+                }
+
+                R.id.nav_calendar -> {
+                    true
+                }
+
+                R.id.nav_notes -> {
+                    true
+                }
+
+                R.id.nav_mood -> {
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
